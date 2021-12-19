9b148f3d28c75465647e6a40b86ff8b5b37280ed
Cheryl Lao <cheryl.lao@outlook.com>
2021-12-19 13:24:39 -0500

    Added more examples of looping through lists

--- session2-lists.py
+++ session2-lists.py
@@ -1,34 +1,57 @@
-# lists
+#!/usr/bin/env python
+# Lists
 
-#lists contain multiple elements
-#Adding to lists
+# lists contain multiple elements
+# Index starts at 0
+
+# Adding to lists
+"""
 list1 = []
 list2 = [1, 2, 3]
+
 list1.append("Hi")
+print(list1)
+
 list1.extend(list2)
+print(list1)
+
 list1.insert(1, "boo")
+print(list1)
+"""
 
-#Removing from a list
+# Removing from a list
+"""
 list1.pop(0)
-list1.remove(2)
-del(list1[3])
+print(list1)
+
+list1.remove(3)
+print(list1)
+
+del(list1[0])
+print(list1)
+
 list1.clear()
+print(list1)
+"""
 
 
 
-
-#lists can contain multiple types of items
+# Lists can contain multiple types of items
 """
 a1 = [1, 2.1, "three"]
-
+c
+# You can loop through the items in the list
 for item in a1:
     print(type(item))
-    if type(item) is str:
-        print("item is a string")
+    
+# The loop does the same thing as above, but you also get a number for the current index
+for i in range(len(a1)):
+    print(type(a1[i]))
 """
 
 
-#Checking if two lists are the same length
+
+# Checking if two lists are the same length
 """
 A = [1, 2, 3, 4, 5]
 B = [5, 4, 3, 2, 1]
@@ -36,7 +59,7 @@ B = [5, 4, 3, 2, 1]
 print(len(A) == len(B))
 """
 
-#How many elements in A are bigger than the corresponding item in B?
+# How many elements in A are bigger than the corresponding item in B?
 """
 count = 0
 for i in range(len(A)):
@@ -46,7 +69,7 @@ print(count)
 """
 
 
-#remove all items of type str in a list
+# Remove all items of type str in a list
 """
 a1 = [1, 2.1, "three"]
 
