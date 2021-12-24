69358e3be1d3073b391fc74340aed45b125b5166
Cheryl Lao <cheryl.lao@outlook.com>
2021-12-23 22:27:52 -0500

    Added first version of session 4 code and some sample CCC solutions from Github

---
+++ session4-subclasses.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+class Book:
+    # A simple example Book class
+
+    # initialization
+    def __init__(self, title, author, pages): # Constructor
+        # Attributes
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    # toString() method
+    # Tells the program how to format a string version of this object nicely
+    def __str__(self):
+        return self.title + " by " + self.author + ". " + str(self.pages) + " pages long"
+
+    # Equals method
+    # Tells the program what conditions makes two of these objects equal
+    def __eq__(self, other_book):
+        return self.title == other_book.title
+
+
+class Bookshelf:
+    def __init__(self, books=[]): # Constructor with a default value for the book list
+        # The books list is empty if it's not specified
+        self.books = books
+
+    # This is an example of a method -- a function that only works for a member of a certain class
+    def shelve_book(self, book):
+        # Check if the item is actually a book before shelving it
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Cannot shelve something that is not a book")
+
+
+# ebook extends Book
+class eBook(Book):
+    def __init__(self, title, author, pages, platform):
+        super().__init__(title, author, pages)
+        self.platform = platform
+
+
+myBook = Book("The Old Man and the Sea", "Ernest Hemingway", 127)
+
+myBook2 = Book("The Old Man and the Sea", "Ernest Hemingway", 127)
+
+#print(myBook == myBook2)
+
+#myeBook = eBook("The Old Man and the Sea", "Ernest Hemingway", 127, "Kindle")
+
+#print(myeBook.platform)
+#print(myBook.platform) #doesn't work
+
+#myBookshelf = Bookshelf()
+#myBookshelf.shelveBook(myeBook)
+#print(myBookshelf.books[0].title)
